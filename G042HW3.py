ae6ea6cb9524b17b536a0bd2f7c8ec56cb043df3
Morippi <Morippi>
2022-05-28 15:01:03 +0200

    finishes????

--- G042HW3.py
+++ G042HW3.py
@@ -84,12 +84,8 @@ def squaredEuclidean(point1, point2):
 # Method euclidean:  euclidean distance
 # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 def euclidean(point1, point2):
-    res = 0
-    for i in range(len(point1)):
-        diff = (point1[i] - point2[i])
-        res += diff * diff
-    return math.sqrt(res)
-
+    dist = [(a - b) ** 2 for a, b in zip(point1, point2)]
+    return math.sqrt(sum(dist))
 
 # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 # Method MR_kCenterOutliers: MR algorithm for k-center with outliers
@@ -110,11 +106,12 @@ def MR_kCenterOutliers(points, k, z, L):
         coresetPoints.append(i[0])
         coresetWeights.append(i[1])
 
+    solution = SeqWeightedOutliers(coresetPoints,coresetWeights,k,z,alpha=2)
     # ****** ADD YOUR CODE
     # ****** Compute the final solution (run SeqWeightedOutliers with alpha=2)
     # ****** Measure and print times taken by Round 1 and Round 2, separately
     # ****** Return the final solution
-
+    return solution
 
 # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 # Method extractCoreset: extract a coreset from a given iterator
@@ -171,86 +168,102 @@ def computeWeights(points, centers):
         weights[mycenter] = weights[mycenter] + 1
     return weights
 
-
 def minDistance(P, n):
-    subset = P[:n]
-    min_d = 99999
-    while subset:
-        i = subset.pop()
+    subset = np.array(P[:n])
+    min_d = float('inf')
+    while len(subset):
+        i = subset[0]
+        subset=np.delete(subset,0,0)
         for j in subset:
-            current_d = euclidean(i, j)
-            if current_d < min_d:
+            #current_d = euclidean(i, j)
+            current_d = np.sqrt(np.sum(np.square(i - j)))
+
+            if  current_d < min_d:
                 min_d = current_d
+    # print(min_d)
     return min_d / 2
 
+def weightInRadius(point_array, weight_array, x, x_w, op):
+    # we used the np to make this step more efficient
+    # we firstly compute the euclidean distances from x to all the point in point_array
+    euclidean_distance = np.sqrt(np.sum(np.square(point_array - x), 1))
 
-def pointsInRadius(P, w, x, r):
-    # returns set of points in radius r from x
-    ball_weight = 0
-    for point in P:
-        if euclidean(point, x) < r:
-            ball_weight += P[point]
-    return ball_weight
+    # we find the indeces of the point that are inside the first ball
+    indeces = np.where(euclidean_distance < op)
+
+    #we get the weight of those points and we remove
+    #the weight of x
+    return weight_array[indeces].sum() - x_w
 
 
 # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 # Method SeqWeightedOutliers: sequential k-center with outliers
 # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 def SeqWeightedOutliers(inputPoints, weights, k, z, alpha=0):
-    # r <- (Min distance between first k+z+1 points) / 2
+    #we compute the first circle base radious
     r = minDistance(inputPoints, k + z + 1)
-    bad1 = 0
-    bad2 = 0
-    bad3 = 0
+    r_init = r;
     num_iter = 1
     while True:
-        Z = {}
-        for i in range(len(inputPoints)):
-            Z[inputPoints[i]] = weights[i]
+        # To make the this function more efficient we are going to use Numpy arrays
+        # to reppresent Z.
+        # Z_weight is going to store the weights of the point in Z, using same indexing
+        Z = np.zeros((len(inputPoints), len(inputPoints[0])))
+        Z_weight = np.zeros(len(inputPoints))
+        for index in range(len(inputPoints)):
+            Z[index] = np.asarray(inputPoints[index])
+            Z_weight[index] = weights[index]
+
+        # We initialize the set of the centers solutions
         S = []
-        W_z = sum(weights)
+        # We compute the initial Weight of Z
+        W_z = np.sum(Z_weight)
+
+        op = (1 + 2 * alpha) * r
         while len(S) < k and W_z > 0:
-            MAX = 0
-            new_center = tuple()
-            start_time = perf_counter()
-            for x in Z:
-                # ball weight is the sum of weights of all point in the radius (1+2*alpha)r
-                ball_weight = pointsInRadius(Z, weights, x, (1 + 2 * alpha) * r)
+            #we initialize max distance and new_center
+            MAX = -1
+            new_center = None
+
+            #for each point we compute the weight inside the relative ball
+            for index in range(len(Z)):
+
+                x = Z[index]
+                x_w = Z_weight[index]
+
+                ball_weight = weightInRadius(Z, Z_weight, x, x_w, op)
                 if ball_weight > MAX:
                     MAX = ball_weight
                     new_center = x
-            S.append(new_center)
-            end_time = perf_counter()
-            bad1 += end_time - start_time
-            start_time = perf_counter()
-            points_to_remove = []
-            for y in Z:
-                if euclidean(y, new_center) < (3 + 4 * alpha) * r:
-                    W_z -= Z[y]
-                    points_to_remove.append(y)
-            end_time = perf_counter()
-            bad2 += end_time - start_time
-            start_time = perf_counter()
-            for point_to_remove in points_to_remove:
-                del Z[point_to_remove]
-            end_time = perf_counter()
-            bad3 += end_time - start_time
+
+            #we add the new center to the solutions
+            S.append(tuple(new_center))
+
+            #we collect the index of the point outside the sedond ball of the new centers
+            points_to_maintain = []
+            for indeces in range(len(Z)):
+                if np.sqrt(np.sum(np.square(Z[indeces] - new_center))) <= ((3 + 4 * alpha) * r):
+                    W_z -= Z_weight[indeces]
+                else:
+                    points_to_maintain.append(indeces)
+
+            # remove points that are not in the bigger circle from Z;
+            Z = Z[points_to_maintain]
+            Z_weight = Z_weight[points_to_maintain]
+
         if W_z <= z:
             break
         else:
             r = 2 * r
             num_iter += 1
-        print(num_iter, W_z, z)
-    print(bad1)
-    print(bad2)
-    print(bad3)
-    return S, r, num_iter
+
+    return S, r_init, r, num_iter
 
 
 # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 # Method computeObjective: computes objective function
 # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-def computeObjective(P, S, z):
+def ComputeObjective(P, S, z):
     # At first we compute for each point the closest center.
     # for each point we save:
     # - the distance to the closest center
